c61e30b1b67e90d6695e870103d9d25efd302eff
项重飞 <luckyaf1993@gmail.com>
2021-10-16 23:36:18 +0800

    ScreenAdapter

--- SmartAndroid/src/main/java/com/luckyaf/smartandroid/utils/AdaptScreenUtils.java
+++ SmartAndroid/src/main/java/com/luckyaf/smartandroid/utils/AdaptScreenUtils.java
@@ -5,64 +5,47 @@ import android.util.DisplayMetrics;
 import android.util.Log;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * 类描述：
- *
- * @author Created by luckyAF on 2021/10/16
+ * <pre>
+ *     @author: Blankj
+ *     blog  : http://blankj.com
+ *     time  : 2016/09/23
+ *     desc  : AdaptScreenUtils
+ * </pre>
  */
 public final class AdaptScreenUtils {
-    private static List<Field> sMetricsFields;
 
-    private AdaptScreenUtils() {
-        throw new UnsupportedOperationException("u can't instantiate me...");
-    }
+    private static boolean isInitMiui = false;
+    private static Field mTmpMetricsField;
 
     /**
-     * Adapt for the horizontal screen, and call it in {@link android.app.Activity#getResources()}.
+     * Adapt for the horizontal screen, and call it in [android.app.Activity.getResources].
      */
-    public static Resources adaptWidth(final Resources resources, final int designWidth) {
-        float newXdpi = (resources.getDisplayMetrics().widthPixels * 72f) / designWidth;
-        applyDisplayMetrics(resources, newXdpi);
+    public static Resources adaptWidth(Resources resources, int designWidth) {
+        DisplayMetrics dm = getDisplayMetrics(resources);
+        float newXdpi = dm.xdpi = (dm.widthPixels * 72f) / designWidth;
+        setAppDmXdpi(newXdpi);
         return resources;
     }
 
     /**
-     * Adapt for the vertical screen, and call it in {@link android.app.Activity#getResources()}.
+     * Adapt for the vertical screen, and call it in [android.app.Activity.getResources].
      */
-    public static Resources adaptHeight(final Resources resources, final int designHeight) {
-        return adaptHeight(resources, designHeight, false);
-    }
-
-    /**
-     * Adapt for the vertical screen, and call it in {@link android.app.Activity#getResources()}.
-     */
-    public static Resources adaptHeight(final Resources resources, final int designHeight, final boolean includeNavBar) {
-        float screenHeight = (resources.getDisplayMetrics().heightPixels
-                + (includeNavBar ? getNavBarHeight(resources) : 0)) * 72f;
-        float newXdpi = screenHeight / designHeight;
-        applyDisplayMetrics(resources, newXdpi);
+    public static Resources adaptHeight(Resources resources, int designHeight) {
+        DisplayMetrics dm = getDisplayMetrics(resources);
+        float newXdpi = dm.xdpi = (dm.heightPixels * 72f) / designHeight;
+        setAppDmXdpi(newXdpi);
         return resources;
-    }
-
-    private static int getNavBarHeight(final Resources resources) {
-        int resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
-        if (resourceId != 0) {
-            return resources.getDimensionPixelSize(resourceId);
-        } else {
-            return 0;
-        }
     }
 
     /**
      * @param resources The resources.
      * @return the resource
      */
-    public static Resources closeAdapt(final Resources resources) {
-        float newXdpi = Resources.getSystem().getDisplayMetrics().density * 72f;
-        applyDisplayMetrics(resources, newXdpi);
+    public static Resources closeAdapt(Resources resources) {
+        DisplayMetrics dm = getDisplayMetrics(resources);
+        float newXdpi = dm.xdpi = dm.density * 72;
+        setAppDmXdpi(newXdpi);
         return resources;
     }
 
@@ -72,7 +55,7 @@ public final class AdaptScreenUtils {
      * @param ptValue The value of pt.
      * @return value of px
      */
-    public static int pt2Px(final float ptValue) {
+    public static int pt2Px(float ptValue) {
         DisplayMetrics metrics = Utils.getApp().getResources().getDisplayMetrics();
         return (int) (ptValue * metrics.xdpi / 72f + 0.5);
     }
@@ -83,67 +66,46 @@ public final class AdaptScreenUtils {
      * @param pxValue The value of px.
      * @return value of pt
      */
-    public static int px2Pt(final float pxValue) {
+    public static int px2Pt(float pxValue) {
         DisplayMetrics metrics = Utils.getApp().getResources().getDisplayMetrics();
         return (int) (pxValue * 72 / metrics.xdpi + 0.5);
     }
 
-    private static void applyDisplayMetrics(final Resources resources, final float newXdpi) {
-        resources.getDisplayMetrics().xdpi = newXdpi;
-        Utils.getApp().getResources().getDisplayMetrics().xdpi = newXdpi;
-        applyOtherDisplayMetrics(resources, newXdpi);
+    private static void setAppDmXdpi(final float xdpi) {
+        Utils.getApp().getResources().getDisplayMetrics().xdpi = xdpi;
     }
 
-    static void preLoad() {
-        applyDisplayMetrics(Resources.getSystem(), Resources.getSystem().getDisplayMetrics().xdpi);
+    private static DisplayMetrics getDisplayMetrics(Resources resources) {
+        DisplayMetrics miuiDisplayMetrics = getMiuiTmpMetrics(resources);
+        if (miuiDisplayMetrics == null) {
+            return resources.getDisplayMetrics();
+        }
+        return miuiDisplayMetrics;
     }
 
-    private static void applyOtherDisplayMetrics(final Resources resources, final float newXdpi) {
-        if (sMetricsFields == null) {
-            sMetricsFields = new ArrayList<>();
-            Class resCls = resources.getClass();
-            Field[] declaredFields = resCls.getDeclaredFields();
-            while (declaredFields != null && declaredFields.length > 0) {
-                for (Field field : declaredFields) {
-                    if (field.getType().isAssignableFrom(DisplayMetrics.class)) {
-                        field.setAccessible(true);
-                        DisplayMetrics tmpDm = getMetricsFromField(resources, field);
-                        if (tmpDm != null) {
-                            sMetricsFields.add(field);
-                            tmpDm.xdpi = newXdpi;
-                        }
-                    }
-                }
-                resCls = resCls.getSuperclass();
-                if (resCls != null) {
-                    declaredFields = resCls.getDeclaredFields();
-                } else {
-                    break;
+    private static DisplayMetrics getMiuiTmpMetrics(Resources resources) {
+        if (!isInitMiui) {
+            DisplayMetrics ret = null;
+            String simpleName = resources.getClass().getSimpleName();
+            if ("MiuiResources".equals(simpleName) || "XResources".equals(simpleName)) {
+                try {
+                    //noinspection JavaReflectionMemberAccess
+                    mTmpMetricsField = Resources.class.getDeclaredField("mTmpMetrics");
+                    mTmpMetricsField.setAccessible(true);
+                    ret = (DisplayMetrics) mTmpMetricsField.get(resources);
+                } catch (Exception e) {
+                    Log.e("AdaptScreenUtils", "no field of mTmpMetrics in resources.");
                 }
             }
-        } else {
-            applyMetricsFields(resources, newXdpi);
+            isInitMiui = true;
+            return ret;
         }
-    }
-
-    private static void applyMetricsFields(final Resources resources, final float newXdpi) {
-        for (Field metricsField : sMetricsFields) {
-            try {
-                DisplayMetrics dm = (DisplayMetrics) metricsField.get(resources);
-                if (dm != null){
-                    dm.xdpi = newXdpi;
-                }
-            } catch (Exception e) {
-                Log.e("AdaptScreenUtils", "applyMetricsFields: " + e);
-            }
+        if (mTmpMetricsField == null) {
+            return null;
         }
-    }
-
-    private static DisplayMetrics getMetricsFromField(final Resources resources, final Field field) {
         try {
-            return (DisplayMetrics) field.get(resources);
+            return (DisplayMetrics) mTmpMetricsField.get(resources);
         } catch (Exception e) {
-            Log.e("AdaptScreenUtils", "getMetricsFromField: " + e);
             return null;
         }
     }
